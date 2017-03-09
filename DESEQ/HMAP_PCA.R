6d6070cb32ba73bf6f4906b6d9132ff384a3bf80
Ayan Malakar <mailme.ayan@gmail.com>
2017-03-09 15:01:28 -0500

    GENERATES NORMALISED AND VARIANCE STABILISED CSV FILES

--- DESEQ/HMAP_PCA.R
+++ DESEQ/HMAP_PCA.R
@@ -3,6 +3,7 @@
 #source("https://bioconductor.org/biocLite.R")
 #biocLite("DESeq")
 library("DESeq")
+setwd("/Users/ayanmalakar/pranjan10182016/DESEQ")
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
   stop("At least 2 arguments must be supplied (input file).n", call. = FALSE)
@@ -15,6 +16,18 @@ sample.pair<-read.csv(item, row.names = 1)
 sample.pair<-sample.pair[rowSums(sample.pair) > 0, ]#Collect genes with counts >0
 pair1<-sep.names[c(1)]
 pair2<-sep.names[c(2)]
+#####loading the cumulative pvalues if they exist#####
+if (file.exists("pval.FC0.cumulative.rds")) {
+  pval.FC0.cumulative=readRDS("pval.FC0.cumulative.rds")
+} else {
+  pval.FC0.cumulative=vector("numeric")
+}
+
+if (file.exists("padj.FC0.cumulative.rds")){
+  padj.FC0.cumulative=readRDS("padj.FC0.cumulative.rds")
+} else {
+  padj.FC0.cumulative=vector("numeric")
+}
 if (substr(pair1, 3, 4) == '-')  {#inspect the 3rd character
   pair1.occur<-grepl(substr(pair1, 1, 2), colnames(sample.pair), perl = "TRUE")
 } else  {
@@ -26,15 +39,40 @@ condition<-factor(c(rep.int(pair1, pair1.no),rep.int(pair2, pair2.no)))
 cds<-newCountDataSet(sample.pair, condition)
 cds<-estimateSizeFactors(cds)
 sizeFactors(cds)
+write.csv(as.data.frame(counts(cds,normalized = TRUE)),file=paste(pair1,pair2,"normalised","csv",sep="."))
 cds<-estimateDispersions(cds)
 str(fitInfo(cds))
 res<-nbinomTest(cds, pair1, pair2)
+####cumulative pval and padj#######
+jpeg(paste(pair1,pair2,"his.pval.FC0.jpeg",sep = "."))
+pval.FC0=ifelse(res$foldChange==0,res$pval,NA)
+pval.FC0=pval.FC0[!is.na(pval.FC0)]
+saveRDS(pval.FC0,paste(pair1,pair2,"pval.FC0.rds",sep="."))
+pval.FC0.cumulative=c(pval.FC0.cumulative,pval.FC0)
+saveRDS(pval.FC0.cumulative,"pval.FC0.cumulative.rds")
+if (length(pval.FC0)!=0) {
+  hist(pval.FC0, breaks = 100, col = "skyblue", border = "slateblue", main=" ")
+}
+dev.off()
+jpeg(paste(pair1,pair2,"his.padj.FC0.jpeg",sep = "."))
+padj.FC0=ifelse(res$foldChange==0,res$padj,NA)
+padj.FC0=padj.FC0[!is.na(padj.FC0)]
+saveRDS(padj.FC0,paste(pair1,pair2,"padj.FC0.rds",sep="."))
+padj.FC0.cumulative=c(padj.FC0.cumulative,padj.FC0)
+saveRDS(padj.FC0.cumulative,"padj.FC0.cumulative.rds")
+if (length(padj.FC0)!=0) {
+  hist(padj.FC0, breaks=100, col = "skyblue", border = "slateblue", main=" ")
+}
+dev.off()
+saveRDS(res, paste(pair1,pair2,"res","rds",sep = "."))###save the RDS file here 
 ###Load the TSV file containing annotations######
 ptrich.allgenes = read.table(file = gene.annotation, header = TRUE, row.names = 1)
 ##select for genes at padjusted value of < 10% FDR####
 res.sig<-res[ res$padj < 0.1, ]
 if (is.na(res.sig[2:nrow(res.sig), ])) {#checking if ALL elements of the data frame is NA or not 
   print("res.sig is NA")
+  saveRDS(res.sig, paste(pair1,pair2,"rds",sep="."))
+  
 }  else {
     res.sig.dwnreg = res.sig[res.sig$foldChange<1,]#Select only genes with fold change <1
     #among the genes with fold change<1,order them from lowest value to highest (break a tie with higher gene expression across samples)
@@ -51,9 +89,10 @@ if (is.na(res.sig[2:nrow(res.sig), ])) {#checking if ALL elements of the data fr
     system(sub_command)
     ####Significantly Upregulated genes##########
     res.sig.upreg = res.sig[res.sig$foldChange>1,]
+    res.sig.upreg = res.sig.upreg[complete.cases(res.sig.upreg), ]
     res.sig.upreg = res.sig.upreg[order(- res.sig.upreg$foldChange, - res.sig.upreg$baseMean), ]
     res.sig.upreg100 = res.sig.upreg[c(1:100), ]#Top 100 upregulated genes
-    ##remove all NA values!##############
+    ##Remove all NA values among significantly upregulated genes!##############
     res.sig.upreg100=res.sig.upreg100[complete.cases(res.sig.upreg100), ]
     upreg100 = rownames(res.sig.upreg100)
     des = as.character(ptrich.allgenes[res.sig.upreg100$id, c(2)])
@@ -62,9 +101,20 @@ if (is.na(res.sig[2:nrow(res.sig), ])) {#checking if ALL elements of the data fr
     write.table(res.sig.upreg100, file = fname)
     sub_command = paste("sed -i.bak 's/_/ /g'",fname, sep = ' ')
     system(sub_command)
+    #######Combining the significantly expressed genes and saving it into R data file#
+    res.sig.combine = res.sig[res.sig$foldChange>1 | res.sig$foldChange<1,]#we are skipping where fold change =1
+    write.csv(res.sig.combine, file = paste(pair1, pair2,"sig","csv",sep = "."))
+    rownames(res.sig.combine)=NULL
+    saveRDS(res.sig.combine, paste(pair1,pair2,"rds",sep="."))
+    ######Combining the 2fold significantly expressed genes and saving it into R data file######
+    res.sig.combine.2fold = res.sig[res.sig$log2FoldChange>=1 | res.sig.combine$log2FoldChange<=-1, ]
+    write.csv(res.sig.combine.2fold, file = paste(pair1, pair2,"sig.2foldChange","csv", sep="."))
+    rownames(res.sig.combine.2fold)=NULL
+    saveRDS(res.sig.combine.2fold, paste(pair1,pair2,"2fold","rds",sep="."))
     #####variance stabilised data########
     cds.blind = estimateDispersions(cds,method="blind")
     vsd.blind = varianceStabilizingTransformation(cds.blind)
+    write.csv(exprs(vsd.blind),file=paste(pair1,pair2,"variance.stabilised","csv",sep="."))
     fname.vsd.up.hmap = paste(pair1, pair2,"up.vsd", "hmap", "jpg", sep = ".")
     fname.norm.up.hmap=paste(pair1, pair2,"up.norm", "hmap", "jpg", sep = ".")
     fname.vsd.dwn.hmap = paste(pair1, pair2,"dwn.vsd", "hmap", "jpg", sep = ".")
